9cb88269b21f92b4b4f5ec26039cf5694bf7c818
Dirk Avery <dirk.avery@gmail.com>
2020-04-13 20:43:22 -0400

    Initial workings

--- winlxchmod/__init__.py
+++ winlxchmod/__init__.py
@@ -5,6 +5,7 @@ Module for working with file permissions.
 """
 
 import os
+import stat
 
 HAS_PYWIN32 = False
 try:
@@ -22,7 +23,6 @@ if HAS_PYWIN32:
         ntsecuritycon.FILE_GENERIC_EXECUTE,
         ntsecuritycon.FILE_GENERIC_WRITE,
         (
-            ntsecuritycon.READ_CONTROL |
             ntsecuritycon.SYNCHRONIZE |
             ntsecuritycon.FILE_GENERIC_WRITE |
             ntsecuritycon.FILE_GENERIC_EXECUTE
@@ -137,10 +137,93 @@ if HAS_PYWIN32:
         )
     ]
 
+
+W_FLDIR = ntsecuritycon.FILE_LIST_DIRECTORY    # =                           1
+W_FADFL = ntsecuritycon.FILE_ADD_FILE          # =                          10
+W_FADSD = ntsecuritycon.FILE_ADD_SUBDIRECTORY  # =                         100
+W_FRDEA = ntsecuritycon.FILE_READ_EA           # =                        1000
+W_FWREA = ntsecuritycon.FILE_WRITE_EA          # =                       10000
+W_FTRAV = ntsecuritycon.FILE_TRAVERSE          # =                      100000
+W_FDLCH = ntsecuritycon.FILE_DELETE_CHILD      # =                     1000000
+W_FRDAT = ntsecuritycon.FILE_READ_ATTRIBUTES   # =                    10000000
+W_FWRAT = ntsecuritycon.FILE_WRITE_ATTRIBUTES  # =                   100000000
+W_DELET = ntsecuritycon.DELETE                 # =           10000000000000000
+W_RDCON = ntsecuritycon.READ_CONTROL           # =          100000000000000000
+W_WRDAC = ntsecuritycon.WRITE_DAC              # =         1000000000000000000
+W_WROWN = ntsecuritycon.WRITE_OWNER            # =        10000000000000000000
+W_SYNCH = ntsecuritycon.SYNCHRONIZE            # =       100000000000000000000
+W_FGNEX = ntsecuritycon.FILE_GENERIC_EXECUTE   # =       100100000000010100000
+W_FGNRD = ntsecuritycon.FILE_GENERIC_READ      # =       100100000000010001001
+W_FGNWR = ntsecuritycon.FILE_GENERIC_WRITE     # =       100100000000100010110
+W_GENAL = ntsecuritycon.GENERIC_ALL            # 10000000000000000000000000000
+W_GENEX = ntsecuritycon.GENERIC_EXECUTE       # 100000000000000000000000000000
+W_GENWR = ntsecuritycon.GENERIC_WRITE        # 1000000000000000000000000000000
+W_GENRD = ntsecuritycon.GENERIC_READ       # -10000000000000000000000000000000
+
+W_DIRRD = W_FLDIR | W_FRDEA | W_FRDAT | W_RDCON | W_SYNCH
+W_DIRWR = W_FADFL | W_FADSD | W_FWREA | W_FDLCH | W_FWRAT | W_DELET | W_RDCON \
+    | W_WRDAC | W_WROWN | W_SYNCH
+W_DIREX = W_FTRAV | W_RDCON | W_SYNCH
+
+W_FILRD = W_FGNRD
+W_FILWR = W_FDLCH | W_DELET | W_WRDAC | W_WROWN | W_FGNWR
+W_FILEX = W_FGNEX
+
+FILE = 0
+DIRECTORY = 1
+
+OBJECT_TYPES = [FILE, DIRECTORY]
+
+OWNER = 0
+GROUP = 1
+OTHER = 2
+
+OWNER_TYPES = [OWNER, GROUP, OTHER]
+
+READ = 0
+WRITE = 1
+EXECUTE = 2
+
+OPER_TYPES = [READ, WRITE, EXECUTE]
+
+STAT_MASKS = [
+    [stat.S_IRUSR, stat.S_IWUSR, stat.S_IXUSR],
+    [stat.S_IRGRP, stat.S_IWGRP, stat.S_IXGRP],
+    [stat.S_IROTH, stat.S_IWOTH, stat.S_IXOTH]
+]
+
+WIN_RWX_PERMS = [
+    [W_FILRD, W_FILWR, W_FILEX],
+    [W_DIRRD, W_DIRWR, W_DIREX]
+]
+
+
 __version__ = "0.1.0"
 
 
-def win_get_owner(path):
+def figure_out_stat():
+    stat.S_ISUID   # Set UID bit
+    stat.S_ISGID   # Set-group-ID bit
+    stat.S_ENFMT   # System V file locking enforcement
+    stat.S_ISVTX   # File in dir can be renamed or deleted by the owner only
+    stat.S_IREAD   # Owner has read permission
+    stat.S_IWRITE  # Owner has write permission
+    stat.S_IEXEC   # Owner has execute permission
+    stat.S_IRWXU   # Mask for file owner permissions
+    stat.S_IRUSR   # Owner has read permission
+    stat.S_IWUSR   # Owner has write permission
+    stat.S_IXUSR   # Owner has execute permission
+    stat.S_IRWXG   # Mask for group permissions
+    stat.S_IRGRP   # Group has read permission
+    stat.S_IWGRP   # Group has write permission
+    stat.S_IXGRP   # Group has execute permission
+    stat.S_IRWXO   # Mask for permissions for others (not in group)
+    stat.S_IROTH   # Others have read permission
+    stat.S_IWOTH   # Others have write permission
+    stat.S_IXOTH   # Others have execute permission
+
+
+def win_get_owner_sid(path):
     """Get the file owner."""
     sec_descriptor = win32security.GetNamedSecurityInfo(
         path, win32security.SE_FILE_OBJECT,
@@ -149,32 +232,28 @@ def win_get_owner(path):
     return sid
 
 
-def win_get_group(path):
+def win_get_group_sid(path):
     """Get the file group."""
     sec_descriptor = win32security.GetNamedSecurityInfo(
         path, win32security.SE_FILE_OBJECT,
         win32security.GROUP_SECURITY_INFORMATION)
-    sid = sec_descriptor.GetSecurityDescriptorGroup()
-    return sid
+    return sec_descriptor.GetSecurityDescriptorGroup()
+
+
+def win_get_other_sid():
+    """Get the other SID.
+
+    For now this is the Users builtin account. In the future, probably should
+    allow account to be passed in and find any non-owner, non-group account
+    currently associated with the file. As a default, it could use Users."""
+    return win32security.LookupAccountName(None, 'Users')[0]
 
 
 def _win_get_idx(mask, access_list):
     """Get the index for the mask in permissions list."""
-    for rindex in range(0, 8):
-        index = 7 - rindex
-        if (mask & access_list[index]) == access_list[index]:
+    for index in range(7, -1, -1):
+        if mask & access_list[index] == access_list[index]:
             return index
-
-
-def win_set_permissions(path, mode):
-    """Set the file or dir permissions."""
-    if not os.path.exists(path):
-        raise FileNotFoundError('Path %s could not be found.' % path)
-
-    if len(mode) != 3:
-        raise AttributeError('Mode must be 3 char long, given %s.' % mode)
-
-    _win_set_permissions(path, mode)
 
 
 def win_get_permissions(path):
@@ -187,91 +266,122 @@ def win_get_permissions(path):
 
 def _win_get_permissions(path):
     """Get the permissions."""
-    if os.path.isfile(path):
-        accesses = WIN_FILE_ACCESS
-    else:
-        accesses = WIN_DIR_ACCESS
+    # if os.path.isfile(path):
+    #     accesses = WIN_FILE_ACCESS
+    # else:
+    #    accesses = WIN_DIR_ACCESS
 
     sec_descriptor = win32security.GetNamedSecurityInfo(
         path, win32security.SE_FILE_OBJECT,
         win32security.DACL_SECURITY_INFORMATION)
     dacl = sec_descriptor.GetSecurityDescriptorDacl()
 
-    owner_sid = win_get_owner(path)
-    group_sid = win_get_group(path)
+    owner_sid = win_get_owner_sid(path)
+    group_sid = win_get_group_sid(path)
 
-    owner_idx = 0
-    group_idx = 0
-    users_idx = 0
-
+    perm = 0
     for index in range(0, dacl.GetAceCount()):
         ace = dacl.GetAce(index)
         if ace[2] == owner_sid:
-            owner_idx = _win_get_idx(ace[1], accesses)
+            if ace[1] & ntsecuritycon.FILE_GENERIC_READ == \
+                    ntsecuritycon.FILE_GENERIC_READ:
+                perm = perm | stat.S_IRUSR
+            if ace[1] & ntsecuritycon.FILE_GENERIC_WRITE == \
+                    ntsecuritycon.FILE_GENERIC_WRITE:
+                perm = perm | stat.S_IWUSR
+            if ace[1] & ntsecuritycon.FILE_GENERIC_EXECUTE == \
+                    ntsecuritycon.FILE_GENERIC_EXECUTE:
+                perm = perm | stat.S_IXUSR
         elif ace[2] == group_sid:
-            group_idx = _win_get_idx(ace[1], accesses)
-        elif ace[2] != owner_sid and ace[2] != group_sid \
-                and win32security.LookupAccountSid(
-                    None, ace[2])[0] != 'SYSTEM':
-            temp_idx = _win_get_idx(ace[1], accesses)
-            print(
-                "Chcecking users:",
-                win32security.LookupAccountSid(None, ace[2]), temp_idx)
-            if temp_idx > users_idx:
-                users_idx = temp_idx
+            if ace[1] & ntsecuritycon.FILE_GENERIC_READ == \
+                    ntsecuritycon.FILE_GENERIC_READ:
+                perm = perm | stat.S_IRGRP
+            if ace[1] & ntsecuritycon.FILE_GENERIC_WRITE == \
+                    ntsecuritycon.FILE_GENERIC_WRITE:
+                perm = perm | stat.S_IWGRP
+            if ace[1] & ntsecuritycon.FILE_GENERIC_EXECUTE == \
+                    ntsecuritycon.FILE_GENERIC_EXECUTE:
+                perm = perm | stat.S_IXGRP
+        elif win32security.LookupAccountSid(None, ace[2])[0] != 'SYSTEM':
+            if ace[1] & ntsecuritycon.FILE_GENERIC_READ == \
+                    ntsecuritycon.FILE_GENERIC_READ:
+                perm = perm | stat.S_IROTH
+            if ace[1] & ntsecuritycon.FILE_GENERIC_WRITE == \
+                    ntsecuritycon.FILE_GENERIC_WRITE:
+                perm = perm | stat.S_IWOTH
+            if ace[1] & ntsecuritycon.FILE_GENERIC_EXECUTE == \
+                    ntsecuritycon.FILE_GENERIC_EXECUTE:
+                perm = perm | stat.S_IXOTH
 
-    print("Mode: ", owner_idx, group_idx, users_idx)
+    print("Mode: ", perm, "hex:", hex(perm))
+    return perm
 
 
 def _win_append_ace(ace_list, sid, access):
-    trustee = {}
-    trustee['MultipleTrustee'] = None
-    trustee['MultipleTrusteeOperation'] = 0
-    trustee['TrusteeForm'] = win32security.TRUSTEE_IS_SID
-    trustee['TrusteeType'] = win32security.TRUSTEE_IS_USER
-    trustee['Identifier'] = sid
+    """Create ACE and append to list of ACEs."""
+    if access > 0:
+        trustee = {}
+        trustee['MultipleTrustee'] = None
+        trustee['MultipleTrusteeOperation'] = 0
+        trustee['TrusteeForm'] = win32security.TRUSTEE_IS_SID
+        trustee['TrusteeType'] = win32security.TRUSTEE_IS_USER
+        trustee['Identifier'] = sid
 
-    ace_list.append({
-        'Trustee': trustee,
-        'Inheritance': win32security.NO_INHERITANCE,
-        'AccessMode': win32security.GRANT_ACCESS,
-        'AccessPermissions': access
-    })
+        ace_list.append({
+            'Trustee': trustee,
+            'Inheritance': win32security.NO_INHERITANCE,
+            'AccessMode': win32security.GRANT_ACCESS,
+            'AccessPermissions': access
+        })
 
 
-def _win_set_permissions(path, mode):
+def win_set_permissions(path, mode):
+    """Set the file or dir permissions."""
+    if not os.path.exists(path):
+        raise FileNotFoundError('Path %s could not be found.' % path)
+
+    object_type = DIRECTORY
+    if os.path.isfile(path):
+        object_type = FILE
+
+    return _win_set_permissions(path, mode, object_type)
+
+
+def _win_get_accesses(mode, user_type, object_type):
+    """Get bitwise permissions for user type."""
+    access = 0
+
+    for oper in OPER_TYPES:
+        if mode & STAT_MASKS[user_type][oper] == STAT_MASKS[user_type][oper]:
+            access = access | WIN_RWX_PERMS[object_type][oper]
+
+    return access
+
+
+def _win_set_permissions(path, mode, object_type):
     """Set the permissions."""
     sec_descriptor = win32security.GetNamedSecurityInfo(
         path, win32security.SE_FILE_OBJECT,
         win32security.DACL_SECURITY_INFORMATION)
     dacl = sec_descriptor.GetSecurityDescriptorDacl()
 
-    # get rid of all ACEs except system's
-    for _ in range(0, dacl.GetAceCount()):
-        dacl.DeleteAce(0)
-
-    if os.path.isfile(path):
-        accesses = WIN_FILE_ACCESS
-    else:
-        accesses = WIN_DIR_ACCESS
-
     new_aces = []
 
-    # add ACE for owner
-    if mode[0] != '0':
-        _win_append_ace(
-            new_aces, win_get_owner(path), accesses[int(mode[0])])
+    for index in range(0, dacl.GetAceCount()):
+        ace = dacl.GetAce(index)
 
-    # add ACE for group
-    if mode[1] != '0':
-        _win_append_ace(
-            new_aces, win_get_group(path), accesses[int(mode[1])])
+        if win32security.LookupAccountSid(None, ace[2])[0] == 'SYSTEM':
+            _win_append_ace(new_aces, ace[2], ace[1])
 
-    # add ACE for others
-    if mode[2] != '0':
-        users_sid = win32security.LookupAccountName('', 'Users')[0]
-        _win_append_ace(
-            new_aces, users_sid, accesses[int(mode[2])])
+    sids = [
+        win_get_owner_sid(path),
+        win_get_group_sid(path),
+        win_get_other_sid()
+    ]
+
+    for user_type, sid in enumerate(sids):
+        _win_append_ace(new_aces, sid, _win_get_accesses(
+            mode, user_type, object_type))
 
     # make it real
     dacl.SetEntriesInAcl(new_aces)
