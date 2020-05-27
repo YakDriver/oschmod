2342e7d953c237eb843ed215ec425d40779ec5d6
Dirk Avery <dirk.avery@gmail.com>
2020-05-27 13:36:22 -0400

    Add test generator, fix effective calc

--- tests/test_oschmod.py
+++ tests/test_oschmod.py
@@ -9,6 +9,8 @@ import stat
 import string
 import time
 
+from random import randrange
+
 import oschmod
 
 
@@ -84,15 +86,21 @@ def test_set_recursive():
     oschmod.set_mode_recursive(topdir, file_mode, dir_mode)
     time.sleep(1)  # modes aren't always ready to go immediately
 
-    # check it out
-    assert oschmod.get_mode(topdir) == dir_mode
-    assert oschmod.get_mode(os.path.join(topdir, 'testdir2')) == dir_mode
-    assert oschmod.get_mode(testdir) == dir_mode
-    assert oschmod.get_mode(os.path.join(topdir, 'file1')) == file_mode
-    assert oschmod.get_mode(os.path.join(testdir, 'file2')) == file_mode
+    mode_dir1 = oschmod.get_mode(topdir)
+    mode_dir2 = oschmod.get_mode(os.path.join(topdir, 'testdir2'))
+    mode_dir3 = oschmod.get_mode(testdir)
+    mode_file1 = oschmod.get_mode(os.path.join(topdir, 'file1'))
+    mode_file2 = oschmod.get_mode(os.path.join(testdir, 'file2'))
 
     # clean up
     shutil.rmtree(topdir)
+
+    # check it out
+    assert mode_dir1 == dir_mode
+    assert mode_dir2 == dir_mode
+    assert mode_dir3 == dir_mode
+    assert mode_file1 == file_mode
+    assert mode_file2 == file_mode
 
 
 def test_symbolic_effective_add():
@@ -253,6 +261,112 @@ def test_symbolic_effective_eq():
     assert oschmod.get_effective_mode(0b110100001, "u=r") == 0b100100001
     assert oschmod.get_effective_mode(0b110100001, "uo=wx") == 0b011100011
 
+    # randomly chosen starting permission = 359
+    assert oschmod.get_effective_mode(0b101100111, "go=") == 0b101000000
+    assert oschmod.get_effective_mode(0b101100111, "ugo=") == 0b000000000
+    assert oschmod.get_effective_mode(0b101100111, "ugo=rwx") == 0b111111111
+    assert oschmod.get_effective_mode(0b101100111, "uo=w") == 0b010100010
+    assert oschmod.get_effective_mode(0b101100111, "=w") == 0b010010010
+    assert oschmod.get_effective_mode(0b101100111, "uo=wx") == 0b011100011
+    assert oschmod.get_effective_mode(0b101100111, "u=r") == 0b100100111
+
+
+def generate_symbolic():
+    """Generate one symbolic representation of a mode modifier."""
+    who = randrange(8)
+    whom = ((who & 0b100 > 0 and "u") or "") + \
+        ((who & 0b010 > 0 and "g") or "") + \
+        ((who & 0b001 > 0 and "o") or "")
+    oper = randrange(3)
+    operation = ((oper == 0 and "+") or "") + \
+        ((oper == 1 and "-") or "") + \
+        ((oper == 2 and "=") or "")
+    perm = randrange(8)
+    perms = ((perm & 0b100 > 0 and "r") or "") + \
+        ((perm & 0b010 > 0 and "w") or "") + \
+        ((perm & 0b001 > 0 and "x") or "")
+    return whom + operation + perms
+
+
+def generate_case(prefix, suffix):
+    """Generate a test case to be solved manually."""
+    symbolic = [generate_symbolic() for _ in range(randrange(1, 4))]
+    symbolics = ",".join(symbolic)
+    return "{0:s}0b{1:09b}, \"{2:s}\"{3:s} == 0b{1:09b}".format(
+        prefix,
+        randrange(512),
+        symbolics,
+        suffix
+    )
+
+
+def generate_cases(count):
+    """Generate test cases to be solved manually and added to tests."""
+    prefix = "assert oschmod.get_effective_mode("
+    suffix = ")"
+    cases = [generate_case(prefix, suffix) for _ in range(count)]
+    print("\n".join(cases))
+
+
+def test_symbolic_multiples():
+    """Check calculation of effective mode from symbolic."""
+    assert oschmod.get_effective_mode(0b000101010, "g-rwx,go=") == 0b000000000
+    assert oschmod.get_effective_mode(0b011001011, "uo-wx,u=x") == 0b001001000
+    assert oschmod.get_effective_mode(0b111101000, "u=rwx,o=x") == 0b111101001
+    assert oschmod.get_effective_mode(0b110101001, "+r,ug=rx") == 0b101101101
+    assert oschmod.get_effective_mode(
+        0b010010000, "go-,go+,u+rw") == 0b110010000
+    assert oschmod.get_effective_mode(0b000101110, "ug-rw,go=x") == 0b000001001
+    assert oschmod.get_effective_mode(
+        0b010110000, "=rwx,=rw,ug-") == 0b110110110
+    assert oschmod.get_effective_mode(
+        0b010001111, "o-rwx,o=rwx,ug-x") == 0b010000111
+    assert oschmod.get_effective_mode(
+        0b100111011, "u-r,o=rwx,ug-wx") == 0b000100111
+    assert oschmod.get_effective_mode(
+        0b111110101, "o=rwx,ugo-,g=rx") == 0b111101111
+    assert oschmod.get_effective_mode(0b010010000, "u=rx") == 0b101010000
+    assert oschmod.get_effective_mode(0b001011111, "=") == 0b000000000
+    assert oschmod.get_effective_mode(0b100011010, "ug-w,uo=rw") == 0b110001110
+    assert oschmod.get_effective_mode(0b111001001, "ug=rw,g-wx") == 0b110100001
+    assert oschmod.get_effective_mode(
+        0b111000000, "u-,uo+rx,go+x") == 0b111001101
+    assert oschmod.get_effective_mode(0b000000000, "u=rx,uo-x") == 0b100000000
+    assert oschmod.get_effective_mode(0b101110101, "uo=rx") == 0b101110101
+    assert oschmod.get_effective_mode(
+        0b111111010, "g-wx,ug=,-x") == 0b000000010
+    assert oschmod.get_effective_mode(0b100011000, "uo+rw") == 0b110011110
+    assert oschmod.get_effective_mode(
+        0b011111000, "ugo+,uo+w,-rwx") == 0b000000000
+    assert oschmod.get_effective_mode(
+        0b000010100, "ug=x,ug=x,g-rx") == 0b001000100
+    assert oschmod.get_effective_mode(0b110101101, "g=rwx") == 0b110111101
+    assert oschmod.get_effective_mode(0b000010111, "=wx") == 0b011011011
+    assert oschmod.get_effective_mode(
+        0b000111011, "u-rw,uo-x,o+wx") == 0b000111011
+    assert oschmod.get_effective_mode(0b010110000, "uo+,u+") == 0b010110000
+    assert oschmod.get_effective_mode(
+        0b000111110, "go=x,ug+x,uo=rx") == 0b101001101
+    assert oschmod.get_effective_mode(0b011101111, "o+wx") == 0b011101111
+    assert oschmod.get_effective_mode(
+        0b001001011, "u-,go+w,ugo=w") == 0b010010010
+    assert oschmod.get_effective_mode(0b110110100, "u=w,=x") == 0b001001001
+    assert oschmod.get_effective_mode(
+        0b110011100, "u=w,ug-rwx,uo+rwx") == 0b111000111
+    assert oschmod.get_effective_mode(0b100101001, "go-r") == 0b100001001
+    assert oschmod.get_effective_mode(
+        0b110100110, "uo=r,ug+rx,ugo=") == 0b000000000
+    assert oschmod.get_effective_mode(0b101100000, "go=wx,o-") == 0b101011011
+    assert oschmod.get_effective_mode(0b111111101, "-r,o=r,o-w") == 0b011011100
+    assert oschmod.get_effective_mode(0b110101000, "uo-rx,+rwx") == 0b111111111
+    assert oschmod.get_effective_mode(0b101011111, "go=wx") == 0b101011011
+    assert oschmod.get_effective_mode(
+        0b110110010, "go+x,ugo-w,u+rwx") == 0b111101001
+    assert oschmod.get_effective_mode(0b001100101, "=rx,-rwx") == 0b000000000
+    assert oschmod.get_effective_mode(0b001010011, "uo=x") == 0b001010001
+    assert oschmod.get_effective_mode(
+        0b011101110, "ugo-rx,uo+rw,uo-rw") == 0b000000000
+
 
 def test_symbolic_use():
     """Check file permissions are recursively set."""
@@ -282,12 +396,21 @@ def test_symbolic_use():
     oschmod.set_mode_recursive(topdir, "u=rw,go=", "u=rwx,go=")
     time.sleep(1)  # modes aren't always ready to go immediately
 
-    # check it out
-    assert oschmod.get_mode(topdir) == 0o700
-    assert oschmod.get_mode(os.path.join(topdir, 'testdir2')) == 0o700
-    assert oschmod.get_mode(testdir) == 0o700
-    assert oschmod.get_mode(os.path.join(topdir, 'file1')) == 0o600
-    assert oschmod.get_mode(os.path.join(testdir, 'file2')) == 0o600
+    dir_mode = 0o700
+    file_mode = 0o600
+
+    mode_dir1 = oschmod.get_mode(topdir)
+    mode_dir2 = oschmod.get_mode(os.path.join(topdir, 'testdir2'))
+    mode_dir3 = oschmod.get_mode(testdir)
+    mode_file1 = oschmod.get_mode(os.path.join(topdir, 'file1'))
+    mode_file2 = oschmod.get_mode(os.path.join(testdir, 'file2'))
 
     # clean up
     shutil.rmtree(topdir)
+
+    # check it out
+    assert mode_dir1 == dir_mode
+    assert mode_dir2 == dir_mode
+    assert mode_dir3 == dir_mode
+    assert mode_file1 == file_mode
+    assert mode_file2 == file_mode
